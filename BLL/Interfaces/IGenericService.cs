a4e35ae2ae7f9b45074883be8704c69a32cb598e
Student <student@lan.itstep.kiev.ua>
2019-04-09 11:41:36 +0300

    Create Bll, create service

---
+++ BLL/Interfaces/IGenericService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IGenericService<DOMAIN, DTO> where DTO : class, new() where DOMAIN : class, new()
+    {
+        IEnumerable<DTO> GetAll();
+        IEnumerable<DTO> FindBy(Expression<Func<DTO, bool>> predicate);
+        DTO Get(int id);
+        DTO Add(DTO obj);
+        DTO Update(DTO obj);
+        DTO Delete(int id);
+    }
+}
