919795e482093fe9390201cd4f4e03615a9141e2
ViktorDem <vicdmdemidov@gmail.com>
2019-04-10 10:15:20 +0300

    Create ServiceModule.cs

---
+++ BLL/Modules/ServiceModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Modules
+{
+    public class ServiceModule : Module
+    {
+        
+    }
+}
