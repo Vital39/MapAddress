a1dbf5301e2b2bc395a78c493e3c24dbb8fdf73b
ViktorDem <vicdmdemidov@gmail.com>
2019-04-10 12:06:12 +0300

    Add empty Load method into ServiceModule.cs

--- BLL/Modules/ServiceModule.cs
+++ BLL/Modules/ServiceModule.cs
@@ -1,4 +1,8 @@
 ﻿using Autofac;
+using BLL.Models;
+using BLL.Services;
+using DAL.DbLayer;
+using Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +13,9 @@ namespace BLL.Modules
 {
     public class ServiceModule : Module
     {
-      
+        protected override void Load(ContainerBuilder builder)
+        {
+       
+        }
     }
 }
