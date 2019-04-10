1246b0d39463dee28dbed2e7bb0ac503250cf0b9
ViktorDem <vicdmdemidov@gmail.com>
2019-04-10 10:08:31 +0300

    Add property into AddressDTO.cs

--- BLL/Models/AddressDTO.cs
+++ BLL/Models/AddressDTO.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Models
 {
-    class AddressDTO
+    public class AddressDTO
     {
+        public int AddressId { get; set; }
 
+        public int StreetId { get; set; }
+
+        public int SubdivisionId { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string House { get; set; }
+
+        [StringLength(24)]
+        public string Serial { get; set; }
+
+        public int? СountFloor { get; set; }
+
+        public int? СountEntrance { get; set; }
+
+        public decimal Latitude { get; set; }
+
+        public decimal Longitude { get; set; }
     }
 }
