4db79aecdd1b074e1af89978bdc71e937aeeedb8
ViktorDem <vicdmdemidov@gmail.com>
2019-04-10 10:01:23 +0300

    Create BLL library, add IGEnericService, AddressDTO

--- DAL/DbLayer/Address.cs
+++ DAL/DbLayer/Address.cs
@@ -4,7 +4,7 @@ namespace DAL.DbLayer
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
+    //using System.Data.Entity.Spatial;
 
     [Table("Address")]
     public partial class Address
