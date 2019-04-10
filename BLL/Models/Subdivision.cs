95fd6a2f3723145437d81db415bfa707bc17a5ec
ViktorDem <vicdmdemidov@gmail.com>
2019-04-10 10:12:03 +0300

    Add SubdivisionDTO.cs with properties

---
+++ BLL/Models/Subdivision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class Subdivision
+    {
+        public int SubdivisionId { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string SubdivisionName { get; set; }
+    }
+}
