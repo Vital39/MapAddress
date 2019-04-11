091acd43e61cd784a116bc2369061fe4bc66646c
Elias Kosogov <i.kosogov@gmail.com>
2019-04-11 09:30:39 +0300

    created ApplicationRoleManager created ApplicationUserManager

---
+++ User.Identity/Manager/ApplicationRoleManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Identity.Model;
+using User.Identity.Store;
+using static User.Identity.Model.AppUser;
+
+namespace User.Identity.Manager
+{
+    public class ApplicationRoleManager : RoleManager<AppRole, int>
+    {
+        public ApplicationRoleManager(IRoleStore<AppRole, int> roleStore)
+            : base(roleStore)
+        {
+
+        }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options,
+                         IOwinContext context)
+        {
+            return new ApplicationRoleManager(new RoleStore<AppRole, int, CustomUserRole>(context.Get<ApplicationDbContext>()));
+        }
+    }
+}
