f4583291672080d3522087f46d80906bce0407a9
Demidron <andrew.demidyuk@gmail.com>
2019-04-14 19:51:24 +0300

    write method Load in ServiceModule

--- BLL/Modules/ServiceModule.cs
+++ BLL/Modules/ServiceModule.cs
@@ -1,13 +1,12 @@
 ﻿using Autofac;
+using BLL.Interfaces;
 using BLL.Models;
 using BLL.Services;
 using DAL.DbLayer;
+using DAL.Repository;
 using Repository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+
 
 namespace BLL.Modules
 {
@@ -15,7 +14,26 @@ namespace BLL.Modules
     {
         protected override void Load(ContainerBuilder builder)
         {
-       
+            //Address
+            builder.RegisterType(typeof(AddressService))
+                        .As(typeof(IGenericService<AddressDTO>));
+            builder.RegisterType(typeof(MainRepository<Address>))
+                      .As(typeof(IGenericRepository<Address>));
+
+            //Street
+            builder.RegisterType(typeof(StreetService))
+                        .As(typeof(IGenericService<StreetDTO>));
+            builder.RegisterType(typeof(MainRepository<Street>))
+                      .As(typeof(IGenericRepository<Street>));
+
+            //Subdivision
+            builder.RegisterType(typeof(SubdivisionService))
+                        .As(typeof(IGenericService<SubdivisionDTO>));
+            builder.RegisterType(typeof(MainRepository<Subdivision>))
+                      .As(typeof(IGenericRepository<Subdivision>));
+
+            builder.RegisterType(typeof(AddressContext))
+                     .As(typeof(DbContext)).InstancePerLifetimeScope();
         }
     }
 }
