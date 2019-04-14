d46b9e214a87d3d0e50cd2f8de47ba460f48660c
Demidron <andrew.demidyuk@gmail.com>
2019-04-14 19:28:55 +0300

    Add address list controller

--- WebUI/Controllers/AddressController.cs
+++ WebUI/Controllers/AddressController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.Models;
+using BLL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,18 @@ namespace WebUI.Controllers
 {
     public class AddressController : Controller
     {
+        IGenericService<AddressDTO> addressService;
+
+        public AddressController(IGenericService<AddressDTO> addressService)
+        {
+            this.addressService = addressService;
+        }
+
         // GET: Address
         public ActionResult Index()
         {
-            return View();
+            var model = addressService.GetAll();
+            return View(model);
         }
     }
 }
