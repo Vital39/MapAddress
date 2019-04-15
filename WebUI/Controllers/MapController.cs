8eaea9fcc7675093b2d82cb882b84146dd33c597
Demidron <andrew.demidyuk@gmail.com>
2019-04-15 12:04:59 +0300

    Add GetAdress to MapController

--- WebUI/Controllers/MapController.cs
+++ WebUI/Controllers/MapController.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using BLL.Interfaces;
+using BLL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     public class MapController : Controller
     {
+        IGenericService<AddressDTO> addressService;
+        IGenericService<StreetDTO> streetService;
+        IGenericService<SubdivisionDTO> subdivisionService;
+
+        public MapController(IGenericService<AddressDTO> addressService, IGenericService<StreetDTO> streetService, IGenericService<SubdivisionDTO> subdivisionService)
+        {
+            this.addressService = addressService;
+            this.streetService = streetService;
+            this.subdivisionService = subdivisionService;
+        }
+
+
+
         // GET: Map
         public ActionResult Index()
         {
@@ -16,6 +32,21 @@ namespace WebUI.Controllers
         public ActionResult Map()
         {
             return View();
+        }
+        public JsonResult GetAddress(string requestStr)
+        {
+            var streets = streetService.FindBy(x => x.StreetName.Contains(requestStr)).ToList();
+            //var addresses = streets.Join(streets, ) (s => s.StreetId == addressService.FindBy(x=>x.StreetId))
+
+            List<string> strs = new List<string>(); /*= addressService.Ge;*/
+
+            foreach (var street in streets)
+            {
+                strs.AddRange(addressService.FindBy(x => x.StreetId == street.StreetId).Select(x=> street.StreetName+" "+x.House));
+            }
+
+           
+            return Json(strs, JsonRequestBehavior.AllowGet);
         }
     }
 }
