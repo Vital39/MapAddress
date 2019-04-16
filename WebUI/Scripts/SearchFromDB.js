37289c4600da8409efb841eb691bf92b00764b8c
Demidron <andrew.demidyuk@gmail.com>
2019-04-16 10:43:49 +0300

    Move js logic in files

---
+++ WebUI/Scripts/SearchFromDB.js
@@ -0,0 +1,27 @@
+﻿//поиск по БД
+$(function () {
+    //function log(message) {
+    //    $("<div>").text(message).prependTo("#log");
+    //    $("#log").scrollTop(0);
+    //}
+
+    $("#travelto").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/Map/GetAddress",
+                type: 'POST',
+                dataType: "json",
+                data: {
+                    requestStr: request.term
+                },
+                success: function (data) {
+                    response(data);
+                }
+            });
+        },
+        minLength: 2,
+        select: function (event, ui) {
+            //log("Selected: " + ui.item.value + " aka " + ui.item.id);
+        }
+    });
+});
