1e98719cc9c2c3db574314438e44d603d55dc63c
Elias Kosogov <i.kosogov@gmail.com>
2019-04-17 22:18:10 +0300

    created AuthController

---
+++ WebUI/Controllers/AuthController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using UserIdentity.Model;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser, int> userManager;
+
+        public AuthController()
+            : this(Startup.UserManagerFactory.Invoke())
+        {
+        }
+
+        public AuthController(UserManager<AppUser, int> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LogInModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(LogInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = userManager.Find(model.Email, model.Password);
+
+            if (user != null)
+            {
+                var identity = userManager.CreateIdentity(
+                    user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                GetAuthenticationManager().SignIn(identity);
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Invalid email or password");
+            return View(model);
+        }
+
+
+        private void SignIn(AppUser user)
+        {
+            var identity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.FirstName));
+            identity.AddClaim(new Claim(ClaimTypes.Surname, user.LastName));
+
+            GetAuthenticationManager().SignIn(identity);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            GetAuthenticationManager().SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+        private IAuthenticationManager GetAuthenticationManager()
+        {
+            var ctx = Request.GetOwinContext();
+            //добавить сборку Microsoft.Owin.Host.SystemWeb - NuGet package, чтобы был доступен GetOwinContext()!!!
+
+            return ctx.Authentication;
+        }
+    }
+}
