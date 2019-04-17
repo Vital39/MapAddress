79dffdc509ca5544a95b3bb98e64d22aa817eb14
Elias Kosogov <i.kosogov@gmail.com>
2019-04-17 20:49:57 +0300

    created class AppRole and class AppUser

---
+++ UserIdentity/Model/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserIdentity.Model
+{
+    public class AppUser : IdentityUser<int, CustomUserLogin, CustomUserRole, CustomUserClaim>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string Email { get; set; }
+    }
+    public class CustomUserRole : IdentityUserRole<int> { }
+    public class CustomUserClaim : IdentityUserClaim<int> { }
+    public class CustomUserLogin : IdentityUserLogin<int> { }
+}
